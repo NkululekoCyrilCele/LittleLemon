f6e7a935acf654f89d8f4e8e52d2302db0da9e10
Nkululeko <nkululeko.cele@gmail.com>
2023-01-25 11:40:51 +0200

    updated the models file

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Booking(models.Model):
+    id = models.SmallIntegerField(default=11).primary_key
+    name = models.CharField(max_length=255)
+    no_of_guests = models.SmallIntegerField(default=6)
+    booking_date = models.DateField(db_index=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Menu(models.Model):
+    id = models.SmallIntegerField(default=5).primary_key
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.SmallIntegerField(default=5)
+
+    def __str__(self):
+        return self.title
