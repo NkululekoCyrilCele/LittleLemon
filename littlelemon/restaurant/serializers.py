dc12062880103b11eeb9d7f8404f0f69792f2e82
Nkululeko <nkululeko.cele@gmail.com>
2023-01-25 15:56:53 +0200

    new changes at project and app levels

---
+++ littlelemon/restaurant/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from .models import Menu, Booking
+from django.contrib.auth.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'groups']
+
+        def group_name(self, person: User):
+            return [x.name for x in person.groups.all()]
+
+
+class MenuSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Menu
+        fields = ['id', 'title', 'price', 'inventory']
+
+
+class BookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Booking
+        fields = '__all__'
