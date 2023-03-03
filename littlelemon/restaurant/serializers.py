c20494585f07a359ec59406315e91524f765eeca
Nkululeko <nkululeko.cele@gmail.com>
2023-03-03 17:23:24 +0200

    Modify serializers

--- littlelemon/restaurant/serializers.py
+++ littlelemon/restaurant/serializers.py
@@ -3,15 +3,6 @@ from .models import MenuItem, Booking
 from django.contrib.auth.models import User
 
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['url', 'username', 'email', 'groups']
-
-        def group_name(self, person: User):
-            return [x.name for x in person.groups.all()]
-
-
 class MenuItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = MenuItem
