53fafc2a21436a32950de1906afa63ab40d40d99
Nkululeko <nkululeko.cele@gmail.com>
2023-03-04 11:34:48 +0200

    Update models, migrations, app-level urls, and test_views

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -5,6 +5,4 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('menu/', views.MenuItemView.as_view(), name='menu_list_create'),
     path('menu/<int:pk>/', views.SingleMenuItemView.as_view(), name='menu_detail'),
-    path('menu/items/', views.MenuItemView.as_view(),
-         {'post': 'create'}, name='menu_create'),
 ]
